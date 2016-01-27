6a0c17c0dd6848974d2ce5e6174c06ee61068d5f
vysnavi <vysnavis02@gmail.com>
2016-01-27 12:42:12 +0530

    Swapping two variables

---
+++ Swapping Method.py
@@ -0,0 +1,9 @@
+x = input('Enter the value of x:  ')
+y = input('Enter the value of y:  ')
+
+temp = x
+x = y
+y = temp
+
+print('Value of x after swapping: {}'.format(x))
+print('Value of y after swapping: {}'.format(y))
